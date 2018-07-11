de5a1d84bdd31844bb663ecf807f6a859a72ca59
danyy <405507582@qq.com>
2018-07-11 15:00:54 +0800

    新增查询接口

--- src/main/java/com/dyy/service/UserService.java
+++ src/main/java/com/dyy/service/UserService.java
@@ -1,9 +1,31 @@
 package com.dyy.service;
 
 import com.dyy.pojo.User;
+import com.dyy.util.Page;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UserService {
 
-    User getUserByUid(Integer userid);
+    User getUserById(int userId);
+    int createUser(User user) ;
+    User findByLogin(User user) ;
+    Page<User> findByParams(User u, int pageNo, int limit) ;
+    int deleteByPrimaryKey(Integer id);
+    int updateByPrimaryKeySelective(User record);
+    int findAllCount(User u) ;
+    List<User> findHotUser() ;
+
+    List<User> findAllByQuery(User user) ;
+
+    /**
+     * 分页查询
+     * @param map
+     * @return
+     */
+    List<User> list(Map<String,Object> map) ;
+
+    Long getTotal(Map<String,Object> map);
 
 }
--- src/main/java/com/dyy/service/impl/UserServiceImpl.java
+++ src/main/java/com/dyy/service/impl/UserServiceImpl.java
@@ -2,23 +2,90 @@ package com.dyy.service.impl;
 
 import com.dyy.dao.UserMapper;
 import com.dyy.pojo.User;
-import com.dyy.pojo.UserExample;
 import com.dyy.service.UserService;
+import com.dyy.util.Page;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class UserServiceImpl implements UserService {
-
     @Autowired
     private UserMapper userMapper;
 
     @Override
-    public User getUserByUid(Integer userid){
-        UserExample example = new UserExample();
-        UserExample.Criteria criteria = example.createCriteria();
-        criteria.andUseridEqualTo(userid);
-        return userMapper.selectByExample(example).get(0);
+    public User getUserById(int userId) {
+        return this.userMapper.selectByPrimaryKey(userId);
     }
 
+    @Override
+    public int createUser(User user) {
+        return this.userMapper.insertSelective(user) ;
+    }
+
+    @Override
+    public User findByLogin(User user) {
+        return userMapper.findByLogin(user) ;
+    }
+
+    /**
+     * 分页查询
+     */
+    @Override
+    public Page<User> findByParams(User u,int pageNo,int limit) {
+        Page<User> page = new Page<User>();
+        page.setPageNo(pageNo);
+        page.setLimit(limit);
+
+        int offset = page.getOffsets();
+        RowBounds rowBound = new RowBounds(offset, limit);
+
+        List<User> users = userMapper.findByParams(u,rowBound);
+        page.setRows(users);
+        int total = userMapper.findAllCount(u) ;
+        page.setTotal(total) ;
+        if(offset >= page.getTotal()){
+            page.setPageNo(page.getTotalPages());
+        }
+        return page ;
+    }
+
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return userMapper.deleteByPrimaryKey(id) ;
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(User record) {
+        return userMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int findAllCount(User u) {
+        // TODO Auto-generated method stub
+        return userMapper.findAllCount(u) ;
+    }
+
+    @Override
+    public List<User> findHotUser() {
+        return userMapper.findHotUser();
+    }
+
+    @Override
+    public List<User> findAllByQuery(User user) {
+        return userMapper.findAllByQuery(user);
+    }
+
+    @Override
+    public List<User> list(Map<String, Object> map) {
+        return userMapper.list(map);
+    }
+
+    @Override
+    public Long getTotal(Map<String, Object> map) {
+        return userMapper.getTotal(map);
+    }
 }
