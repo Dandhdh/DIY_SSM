f50533472efcafe60da55d315af750a087ad49db
danyy <405507582@qq.com>
2018-07-12 09:58:43 +0800

    全文检索lucene

--- src/main/java/com/dyy/controller/UserController.java
+++ src/main/java/com/dyy/controller/UserController.java
@@ -1,23 +1,174 @@
 package com.dyy.controller;
 
+import com.dyy.lucene.Indexer;
+import com.dyy.lucene.Searcher;
+import com.dyy.pojo.PageEntity;
 import com.dyy.pojo.User;
 import com.dyy.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-
-    @Autowired
+    @Resource
     private UserService userService;
 
-    @RequestMapping(value = "/f",  method = RequestMethod.POST)
-    @ResponseBody
-    public User findUser(Integer userid)throws Exception{
-        User user = userService.getUserByUid(userid);
-        return user;
+    @RequestMapping("/frontUserSet/{id}")
+    public String frontUserSet(@PathVariable int id, Model model) {
+        /*User user = userService.getUserById(id);
+        model.addAttribute("currentUser", user);
+        String img_id = user.getImg_id();
+        if (img_id != null) {
+            String path = imgService.selectByPrimaryKey(Integer.parseInt(img_id)).getPath();
+            model.addAttribute("headimg", path);
+        }*/
+        return "front/user/userSet";
     }
+
+    @RequestMapping("/showUser")
+    public String toIndex(HttpServletRequest request, Model model) {
+        int userId = Integer.parseInt(request.getParameter("id"));
+        User user = this.userService.getUserById(userId);
+        model.addAttribute("user", user);
+        return "showUser";
+    }
+
+
+    @RequestMapping("/showUser/{id}")
+    public String showUser(@PathVariable int id, HttpServletRequest request) {
+        User u = userService.getUserById(id);
+        request.setAttribute("user", u);
+        return "showUser";
+    }
+
+    @RequestMapping("/turnToIndex")
+    public String turnToIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/turnToUserList")
+    public String turnToUserList() {
+        return "user/userList";
+    }
+
+
+    @RequestMapping("/queryUser")
+    public String queryUser(String q) throws Exception {
+        q = new String(q.getBytes("ISO-8859-1"), "UTF-8");
+        User u = new User() ;
+        List<User> users = userService.findAllByQuery(u) ;
+        List<Integer> ids = new ArrayList<Integer>();
+        List<String> city = new ArrayList<String>() ;
+        List<String> descs = new ArrayList<String>() ;
+        for (User user : users){
+            ids.add(user.getUserId()) ;
+            city.add(user.getUsername()) ;
+            descs.add(user.getDescription()) ;
+        }
+        Indexer indexer = new Indexer() ;
+        Integer[] id = (Integer[])ids.toArray(new Integer[ids.size()]);
+        String[] citys = (String[])city.toArray(new String[city.size()]);
+        String[] desc = (String[])descs.toArray(new String[descs.size()]);
+        indexer.setIds(id);
+        indexer.setCitys(citys);
+        indexer.setDescs(desc);
+        indexer.index("E:\\Lucene");
+        String indexDir="E:\\Lucene";
+        try {
+            Searcher.search(indexDir,q);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null ;
+    }
+
+    @RequestMapping("/getUserList")
+    public void getUserList(@ModelAttribute User user, HttpServletResponse response, int page, int rows) {
+        /*response.setCharacterEncoding("utf-8");
+        Page<User> users = userService.findByParams(user,page,rows) ;
+        for(User u :users.getRows()){
+            //将角色ID转换为角色名称
+            String role_id = u.getRole_id() ;
+            if(role_id != null){
+                Role role = roleService.selectByPrimaryKey(Integer.parseInt(role_id)) ;
+                u.setRolename(role.getRole_name()) ;
+            }
+            Date date = u.getLast_date() ;
+            if(date != null){
+                SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss") ;
+                String strDate = sm.format(date) ;
+                u.setParsedate(strDate) ;
+            }
+        }
+
+        String json = JSON.toJSONString(users) ;
+        try {
+            response.getWriter().print(json) ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+    }
+
+    /**
+     * @param @param user
+     * @param @param request
+     * @return void
+     * @throws IOException
+     * @throws
+     * @Description: 新建用户
+     * @author chj
+     * @date 2016-1-2  下午11:06:27
+     */
+    @RequestMapping("/create")
+    public void createUser(User user, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            userService.createUser(user);
+            response.getWriter().print("true");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().print("false");
+        }
+    }
+
+    @RequestMapping("/delete")
+    public void delete(String ids, HttpServletResponse response) throws IOException {
+        String[] str_ids = ids.split(",");
+        for (String id : str_ids) {
+            userService.deleteByPrimaryKey(Integer.parseInt(id));
+            response.getWriter().print("true");
+        }
+    }
+
+    @RequestMapping("/edit")
+    public void edit(User user, HttpServletResponse response) {
+        try {
+            userService.updateByPrimaryKeySelective(user);
+            response.getWriter().print("true");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping("/loginOut")
+    public String loginOut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("user");
+        return "redirect:../login.jsp";
+    }
+
+
 }
---
+++ src/main/java/com/dyy/lucene/Searcher.java
@@ -0,0 +1,67 @@
+package com.dyy.lucene;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.StringReader;
+import java.nio.file.Paths;
+
+/**
+ * 全文索引查询测试类
+ */
+public class Searcher {
+
+    public static void search(String indexDir,String q)throws Exception{
+        Directory dir=FSDirectory.open(Paths.get(indexDir));
+        IndexReader reader=DirectoryReader.open(dir);
+        IndexSearcher is=new IndexSearcher(reader);
+        // Analyzer analyzer=new StandardAnalyzer(); // 标准分词器
+        SmartChineseAnalyzer analyzer=new SmartChineseAnalyzer();
+        QueryParser parser=new QueryParser("desc", analyzer);   // 在索引desc处查询
+        Query query=parser.parse(q);
+        long start=System.currentTimeMillis();
+        TopDocs hits=is.search(query, 10);
+        long end=System.currentTimeMillis();
+        System.out.println("匹配 "+q+" ，总共花费"+(end-start)+"毫秒"+"查询到"+hits.totalHits+"个记录");
+
+        QueryScorer scorer=new QueryScorer(query);
+        Fragmenter fragmenter=new SimpleSpanFragmenter(scorer);
+        // 将匹配字段标记为红色
+        SimpleHTMLFormatter simpleHTMLFormatter=new SimpleHTMLFormatter("<b><font color='red'>","</font></b>");
+        Highlighter highlighter=new Highlighter(simpleHTMLFormatter, scorer);
+        highlighter.setTextFragmenter(fragmenter);
+        for(ScoreDoc scoreDoc:hits.scoreDocs){
+            Document doc=is.doc(scoreDoc.doc);
+            System.out.println(doc.get("city"));
+            System.out.println(doc.get("desc"));
+            String desc=doc.get("desc");
+            if(desc!=null){
+                TokenStream tokenStream=analyzer.tokenStream("desc", new StringReader(desc));
+                System.out.println(highlighter.getBestFragment(tokenStream, desc));
+            }
+        }
+        reader.close();
+    }
+
+    public static void main(String[] args) {
+        String indexDir="F:\\lucene";
+        String q="南京";
+        try {
+            search(indexDir,q);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
